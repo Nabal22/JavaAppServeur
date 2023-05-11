908a8af66885da3f6d84323a29f8143b11f52d65
Nabal22 <alban.talagrand@gmail.com>
2023-05-11 17:45:47 +0200

    BDD Connection

--- src/ConnectionBD.java
+++ src/ConnectionBD.java
@@ -2,18 +2,18 @@ import java.sql.*;
 
 public class ConnectionBD {
     public static void main(String[] args) throws ClassNotFoundException {
-        Class.forName("com.mysql.jdbc.Driver");
+        Class.forName("com.mysql.cj.jdbc.Driver");
 
-        String url = "jdbc:mysql://localhost:3306/testjdbc";
-        String user = "root";
-        String password = "";
+        String url = "jdbc:mysql://db4free.net:3306/java_app_serveur?characterEncoding=UTF-8&useSSL=false";
+        String user = "albantalagrand";
+        String password = "rootpassword";
         Connection conn = null;
 
         try{
             conn = DriverManager.getConnection(url, user, password);
             System.out.println("Connexion réussie !");
         } catch (Exception e) {
-            System.out.println("Connexion échouée !");
+            System.out.println("Connexion échouée !"+ e);
         }
 
     }
